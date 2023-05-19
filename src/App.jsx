587d5d133d0c8657a1eec609ba8d59c728d3966e
ahmed awad <ahmeedsauce7@gmail.com>
2023-05-19 07:19:49 +0300

    linked and ready for auth

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
+
+function App() {
+  return (
+    <Routes>
+    <Route path="/" element={< HomePage />} />
+    <Route path='/signup' element={<SignupPage />} />
+    <Route path='/login' element={<LoginPage />} />
+    <Route path='/profile' element={<ProfilePage />}/>
+  </Routes>
+  )
+}
+
+export default App
